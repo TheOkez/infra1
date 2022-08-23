a61db9760ee9f2f2f1e3f82afa578e0928154afc
Mona Lisa <kezsam52@gmail.com>
2022-08-23 22:14:20 +0100

    2nd  commit

--- main.tf
+++ main.tf
@@ -16,6 +16,8 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
+  #modified the AMI
+  #ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
